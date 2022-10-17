dc18c12476f10af31d5ef00151b0b77cfed81b35
Cees van Kemenade <cees.van.kemenade@gmail.com>
2022-10-17 20:12:01 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,225 @@
+
+use aws_sdk_s3::types::ByteStream;
+use aws_sdk_s3::{Client, Error, Region};
+use aws_config::meta::region::RegionProviderChain;
+use std::io::{Read, Result as IOResult};
+use std::time::Instant;
+use std::any::type_name;
+use std::ptr;
+use std::cmp;
+use bytes::Bytes;
+use futures::executor::block_on;
+
+pub const REGION: &str = "eu-central-1";
+
+mod s3_service;
+
+struct ObjBlock {
+    start: usize,
+    last_used: Instant,
+    data: Bytes
+}
+
+pub struct s3_file {
+    client: Client,
+    pub bucket: String,
+    pub object: String,
+    pub position: usize,
+    block_size: usize,
+    length: Option<usize>,
+    cache: Vec<ObjBlock>
+}
+
+async fn get_client() -> Client {
+    let region_provider = RegionProviderChain::first_try(Region::new(REGION));
+    let region = region_provider.region().await.unwrap();
+
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    Client::new(&shared_config)
+}
+
+impl s3_file {
+
+    
+    // create a new S3_file with an LRU-cache to support fast (sequential) read operations
+    pub fn new(bucket: String, object: String, block_size: usize) -> Self {
+        Self{client: block_on(get_client()), 
+            bucket, 
+            object, 
+            position: 0,
+            block_size,
+            length: None, 
+            cache: Vec::<ObjBlock>::with_capacity(10)}
+    }
+
+    // free the Least Recent Used page to make more room in the cache
+    fn free_lru(&mut self) {
+        if self.cache.len() < 1 {
+            panic!("No block to free");
+        }
+        let mut oldest = Instant::now();
+        let mut oldest_idx = 0_usize;
+
+        for (idx, ob) in self.cache.iter().enumerate() {
+            if ob.last_used < oldest {
+                oldest = ob.last_used.clone();
+                oldest_idx = idx;
+            }
+        }
+        self.cache.remove(oldest_idx);
+    }
+
+    // fn get_length(&mut self) -> usize {
+    //     self.length.get_or_insert_with(|| s3_service::get_length())
+    // }
+
+    // append a block to the cache that contains the byte start and append it to the cache
+    fn get_block_from_store(&mut self, start: usize) -> usize {
+        let block_start = (start / self.block_size) * self.block_size;
+        //let end_block = cmp::min(block_start + self.block_size, self.get_length());
+        let block_end = block_start + self.block_size;
+
+        // create the block and fill it with data
+        let range = format!("bytes={block_start}-{block_end}");
+        println!("\n\n=====================\nAbout to fetch object {}::{} for range='{}'", &self.bucket, &self.object, &range);
+        let get_obj_output = block_on(s3_service::download_object(&self.client, &self.bucket, &self.object, Some(range)));
+        println!("Received object {:?}", get_obj_output);
+        // set length when nog readily available, as we get this information free of charge here.
+        _ = self.length.get_or_insert(get_obj_output.content_length() as usize);
+        let agg_bytes = block_on(get_obj_output.body.collect()).expect("Failed to read data");
+        // turn into bytes and take a (ref-counted) full slice out of it (reuse of same buffer)
+        // Operating on AggregatedBytes directy would be more memory efficient (however, working with non-continguous memory in that case)
+        let data = agg_bytes.into_bytes();
+        let mut new_block = ObjBlock {
+            start: block_start,
+            last_used: Instant::now(),
+            data
+        };
+        self.cache.push(new_block);
+        self.cache.len() - 1
+    }
+
+    // find the block in cache that contains 'start' and read from s3 if needed. Returns the index of the block in the 'cache'.
+    fn find_cached_block(&mut self, start: usize) -> usize {
+        for (idx, ob) in self.cache.iter().enumerate() {
+            if ob.start <= start && start < ob.start + ob.data.len() as usize {
+                return idx
+            } 
+        }
+        // block is not loaded yet
+        if self.cache.len() >= self.cache.capacity() {
+            self.free_lru();
+        };
+
+        self.get_block_from_store(start)
+    }
+
+    // get the filled cache-block and fill up the buffer over to at most 'max_len' bytes. Return the number of read bytes.
+    fn read_segment(&mut self, buffer: &mut[u8], max_len: usize) -> usize {
+        let block_idx = self.find_cached_block(self.position);
+        let block = &self.cache[block_idx];
+        let relative_position = self.position - block.start;
+        let read_len = cmp::min(max_len, block.data.len() - relative_position + 1);
+
+        let src_slice = block.data.slice(relative_position..relative_position+read_len);
+        buffer.copy_from_slice(&src_slice);
+        self.position += read_len;
+        // copy data
+        // let src_ptr = block.data.slice(relative_position..relative_position+read_len).as_ptr();
+        // let dst_ptr = buffer.as_mut_ptr();
+        // unsafe { ptr::copy_nonoverlapping(src_ptr, dst_ptr, read_len) };
+        // see example in: https://doc.rust-lang.org/std/ptr/fn.copy_nonoverlapping.html why next line is adviced
+        //dst_ptr.set_len(read_len);
+
+        read_len
+    }
+}
+
+impl Read for s3_file {
+    fn read(&mut self, buff: &mut [u8]) -> IOResult<usize> {
+        let buff_len = buff.len();
+        let mut read_len = 0;
+        let mut window: &mut  [u8] = buff;
+        while buff_len - read_len > 0 {
+            let len = self.read_segment(window, buff_len - read_len);
+            //shift the window forward (position has been updated already)
+            window = &mut window[len..];
+            read_len += len;
+        }
+        Ok(read_len)
+    }
+}
+
+
+// temporarily included to test from MAIN
+//#[cfg(test)]
+pub mod tests {
+
+    use aws_config::meta::region::RegionProviderChain;
+    use aws_sdk_s3::{Client, Error, Region};
+    //use aws_smithy_http::byte_stream::{ByteStream, AggregatedBytes};
+    use uuid::Uuid;
+    use futures::executor::block_on;
+    use std::io::Read;
+
+    use crate::s3_service;
+    use crate::{s3_file, REGION};
+    
+    async fn setup() -> (Region, Client, String, String, String, String) {
+        let region_provider = RegionProviderChain::first_try(Region::new(REGION));
+        let region = region_provider.region().await.unwrap();
+    
+        let shared_config = aws_config::from_env().region(region_provider).load().await;
+        let client = Client::new(&shared_config);
+    
+        let bucket_name = format!("{}{}", "doc-example-bucket-", Uuid::new_v4().to_string());
+        let file_name = "./test_upload.txt".to_string();
+        let key = "test file key name".to_string();
+        let target_key = "target_key".to_string();
+    
+        (region, client, bucket_name, file_name, key, target_key)
+    }
+
+
+    pub async fn read_from_s3_aux(test_data: &[u8]) -> (Box<[u8]>, Box<[u8]>,Box<[u8]>) {
+        let (region, client, bucket_name, file_name, object_name, target_key) = setup().await;
+        println!("About to create {bucket_name}.");
+
+        s3_service::create_bucket(&client, &bucket_name, region.as_ref()).await.expect("Failed to create bucket");
+    
+        // create the file for testing
+
+        println!("About to create object {bucket_name}::{object_name}.");
+        s3_service::upload_object(&client, &bucket_name, &file_name, &object_name, test_data).await.expect("Failed to create Object in bucket");
+        println!("Created s3-object");
+
+        // test 1
+        let mut s3file_1 = s3_file::new(bucket_name.to_owned(), object_name.clone(), 100);
+
+        println!("Read s3-object");
+        let buff_len = 10;
+        let mut buff1: Box<[u8]> = vec![0;buff_len].into_boxed_slice();
+        let mut buff2: Box<[u8]> = vec![0;buff_len+7].into_boxed_slice();
+        let mut buff3: Box<[u8]> = vec![0;buff_len].into_boxed_slice();
+
+        // move position to 20  (start of "Hello World")
+        s3file_1.position = 10;
+        s3file_1.read(&mut buff1).expect("Failed to read S3-object (buff_1)");
+        s3file_1.read(&mut buff2).expect("Failed to read S3-object (buff_2)");
+        s3file_1.read(&mut buff3).expect("Failed to read S3-object (buff_3)");
+
+        (buff1, buff2, buff3)
+    }
+    
+    #[tokio::test]
+    //#[test]
+    async fn read_from_s3() {
+        //write a dummy_test_file to a bucket
+        //let results = block_on(read_from_s3_aux(s3_service::UPLOAD_CONTENT));
+        println!("About to enter async function.");
+        let results = read_from_s3_aux(s3_service::UPLOAD_CONTENT).await;
+        
+        assert_eq!(results.0.as_ref(), b"0123456789");
+        assert_eq!(results.1, 123);
+    }
+}
---
+++ src/s3_service.rs
@@ -0,0 +1,165 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+// snippet-start:[rust.example_code.s3.scenario_getting_started.lib]
+
+use aws_sdk_s3::model::{
+    BucketLocationConstraint, CreateBucketConfiguration, Delete, ObjectIdentifier,
+};
+use aws_sdk_s3::output::{GetObjectOutput, ListObjectsV2Output};
+use aws_sdk_s3::types::ByteStream;
+use aws_sdk_s3::{Client, Error};
+use std::path::Path;
+use std::str;
+
+// snippet-start:[rust.example_code.s3.basics.delete_bucket]
+pub async fn delete_bucket(client: &Client, bucket_name: &str) -> Result<(), Error> {
+    client.delete_bucket().bucket(bucket_name).send().await?;
+    println!("Bucket deleted");
+    Ok(())
+}
+// snippet-end:[rust.example_code.s3.basics.delete_bucket]
+
+// snippet-start:[rust.example_code.s3.basics.delete_objects]
+pub async fn delete_objects(client: &Client, bucket_name: &str) -> Result<(), Error> {
+    let objects = client.list_objects_v2().bucket(bucket_name).send().await?;
+
+    let mut delete_objects: Vec<ObjectIdentifier> = vec![];
+    for obj in objects.contents().unwrap_or_default() {
+        let obj_id = ObjectIdentifier::builder()
+            .set_key(Some(obj.key().unwrap().to_string()))
+            .build();
+        delete_objects.push(obj_id);
+    }
+    client
+        .delete_objects()
+        .bucket(bucket_name)
+        .delete(Delete::builder().set_objects(Some(delete_objects)).build())
+        .send()
+        .await?;
+
+    let objects: ListObjectsV2Output = client.list_objects_v2().bucket(bucket_name).send().await?;
+    match objects.key_count {
+        0 => Ok(()),
+        _ => Err(Error::Unhandled(Box::from(
+            "There were still objects left in the bucket.",
+        ))),
+    }
+}
+// snippet-end:[rust.example_code.s3.basics.delete_objects]
+
+// snippet-start:[rust.example_code.s3.basics.list_objects]
+pub async fn list_objects(client: &Client, bucket_name: &str) -> Result<(), Error> {
+    let objects = client.list_objects_v2().bucket(bucket_name).send().await?;
+    println!("Objects in bucket:");
+    for obj in objects.contents().unwrap_or_default() {
+        println!("{:?}", obj.key().unwrap());
+    }
+
+    Ok(())
+}
+// snippet-end:[rust.example_code.s3.basics.list_objects]
+
+// snippet-start:[rust.example_code.s3.basics.copy_object]
+pub async fn copy_object(
+    client: &Client,
+    bucket_name: &str,
+    object_key: &str,
+    target_key: &str,
+) -> Result<(), Error> {
+    let mut source_bucket_and_object: String = "".to_owned();
+    source_bucket_and_object.push_str(bucket_name);
+    source_bucket_and_object.push('/');
+    source_bucket_and_object.push_str(object_key);
+
+    client
+        .copy_object()
+        .copy_source(source_bucket_and_object)
+        .bucket(bucket_name)
+        .key(target_key)
+        .send()
+        .await?;
+
+    Ok(())
+}
+// snippet-end:[rust.example_code.s3.basics.copy_object]
+
+// snippet-start:[rust.example_code.s3.basics.download_object]
+// snippet-start:[rust.example_code.s3.basics.get_object]
+pub async fn download_object(client: &Client, bucket_name: &str, key: &str, range: Option<String>) -> GetObjectOutput {
+    let prep_resp = client
+        .get_object()
+        //.range("bytes=20-".to_owned())
+        .set_range(range)
+        .bucket(bucket_name)
+        .key(key);
+    println!("\nPrepared Download request = {:?}\n", &prep_resp);
+    let resp = prep_resp    
+        .send()
+        .await;
+    resp.unwrap()
+}
+// snippet-end:[rust.example_code.s3.basics.get_object]
+// snippet-end:[rust.example_code.s3.basics.download_object]
+
+
+pub const UPLOAD_CONTENT: &[u8] = b"0123456789
+abcdefgh
+Hello world!
+
+And a whole lot more information:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla dolor, varius vel vehicula vitae, dapibus id tellus. Vivamus nisl risus, pretium in nisi non, venenatis rhoncus ligula. Sed pharetra nibh nulla. Integer vitae mollis nisi. Nunc ante nulla, cursus id dolor sit amet, suscipit molestie est. Vestibulum sollicitudin fermentum arcu ut dictum. Sed finibus feugiat libero, sit amet fermentum ex consequat auctor. Donec varius fringilla sagittis. Sed gravida efficitur erat et viverra. Nulla malesuada metus vitae lacus malesuada, at faucibus velit tincidunt. Cras fermentum blandit purus. Integer rutrum semper lorem, rutrum aliquet elit egestas ac. Integer tincidunt sem nec turpis aliquet consectetur. Nunc accumsan est leo, ut tincidunt turpis interdum in. Ut finibus nibh et ullamcorper pharetra.
+Vivamus quis dictum ipsum. Proin congue vulputate elit rhoncus congue. Mauris elementum libero mollis, mattis nibh eu, commodo erat. Duis sodales feugiat diam, ut tempor purus viverra eu. Vestibulum sodales sit amet eros quis placerat. Phasellus venenatis condimentum nisl eget tristique. Suspendisse potenti. Cras a orci nec elit pulvinar condimentum at et diam. Mauris faucibus aliquam posuere. Donec accumsan, metus ac scelerisque iaculis, felis enim ullamcorper ante, quis tristique elit justo et ex. Donec et velit pharetra, viverra lacus vitae, laoreet elit.
+Duis sodales odio velit, nec ornare leo venenatis ut. Pellentesque in libero sit amet libero vestibulum lobortis. Vivamus laoreet ex eget mi suscipit, vitae volutpat felis iaculis. Etiam rhoncus ac arcu nec commodo. Phasellus posuere, mi eget egestas tincidunt, orci tellus placerat turpis, sit amet blandit nibh magna eu est. Nunc ultrices tincidunt rhoncus. Praesent faucibus id augue quis laoreet. Maecenas ac pellentesque eros, id pellentesque magna. Integer faucibus auctor ante. Mauris vitae pharetra erat. Nulla facilisi. Fusce ac ex libero. Duis posuere lacus lectus, ut varius sapien efficitur eget. Nunc enim urna, congue non tristique id, consequat eget nisi. Nulla in massa sit amet nisi rhoncus tempor sagittis id erat.
+Donec in accumsan odio. Integer faucibus velit posuere sem commodo tincidunt. Fusce facilisis ex eget nisl feugiat pulvinar. Curabitur dolor diam, tempus in ligula ut, feugiat cursus elit. Sed et neque molestie, vehicula diam sed, ultricies justo. Maecenas rutrum pharetra sapien eu interdum. Donec vulputate, massa quis malesuada eleifend, ex arcu viverra magna, sit amet volutpat sem risus sit amet dui. Sed ultricies at eros at consequat. Morbi fringilla tristique mauris vel iaculis. Pellentesque ornare dictum finibus. Fusce aliquet odio a blandit interdum.
+Donec mollis finibus metus in cursus. In blandit ornare purus. Vestibulum a ipsum diam. Curabitur vel iaculis diam, id egestas ligula. Morbi condimentum imperdiet tellus. Aenean ut ligula nulla. Nam quis auctor odio. Sed eget blandit magna, sit amet consectetur ex. Quisque fermentum nunc at nisi dictum molestie. Ut tempus fermentum ipsum, vel aliquet sem rutrum quis. Nunc nec tristique diam.
+Touch test.";
+
+
+
+// snippet-start:[rust.example_code.s3.basics.upload_object]
+// snippet-start:[rust.example_code.s3.basics.put_object]
+pub async fn upload_object(
+    client: &Client,
+    bucket_name: &str,
+    file_name: &str,
+    key: &str,
+    body: &[u8]
+) -> Result<(), Error> {
+//    let body = ByteStream::from_path(Path::new(file_name)).await;
+    let body = ByteStream::from_static(UPLOAD_CONTENT);
+    client
+        .put_object()
+        .bucket(bucket_name)
+        .key(key)
+        //.body(body.unwrap())
+        .body(body)
+        .send()
+        .await?;
+
+    println!("Uploaded file: {}", file_name);
+    Ok(())
+}
+// snippet-end:[rust.example_code.s3.basics.put_object]
+// snippet-end:[rust.example_code.s3.basics.upload_object]
+
+// snippet-start:[rust.example_code.s3.basics.create_bucket]
+pub async fn create_bucket(client: &Client, bucket_name: &str, region: &str) -> Result<(), Error> {
+    let constraint = BucketLocationConstraint::from(region);
+    let cfg = CreateBucketConfiguration::builder()
+        .location_constraint(constraint)
+        .build();
+    client
+        .create_bucket()
+        .create_bucket_configuration(cfg)
+        .bucket(bucket_name)
+        .send()
+        .await?;
+    println!("Creating bucket named: {bucket_name}");
+    Ok(())
+}
+// snippet-end:[rust.example_code.s3.basics.create_bucket]
+// snippet-end:[rust.example_code.s3.scenario_getting_started.lib]
